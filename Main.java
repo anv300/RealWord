cf1b3ae2527814457189df0741cc968a09e04319
Anvar Urasin <urasin.anvar@institutmariaespinalt.cat>
2021-02-25 11:55:56 +0000

    player list

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main {
   public static Screen screen = new MenuScreen();
 
   public static void setScreen(Screen screen1) {
+    System.out.println("Switching to screen " + screen1.getClass().getSimpleName());
     screen.clear();
     screen = screen1;
     screen1.draw();
--- PlayerScreen.java
+++ PlayerScreen.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.awt.Color;
+import java.awt.event.*;
 
 public class PlayerScreen extends Screen {
   public List<String> players = new ArrayList<>();
@@ -9,15 +11,81 @@ public class PlayerScreen extends Screen {
     super(new JFrame(), prevScreen);
   }
 
+  JList playlistB;
+
+  protected JList drawPlayerList() {
+    if(playlistB != null) playlistB.setVisible(false);
+    DefaultListModel dlm = new DefaultListModel();
+    for(String player : players) {
+      dlm.addElement(player);
+    } 
+    JScrollPane scrollPane = new JScrollPane();
+    JList playersList = new JList(dlm);
+    // JPanel playersList = new JPanel(); 
+    // playersList.setBounds(30,80,320,100);
+    playersList.setBackground(Color.gray);
+    // playersList.setLayout(new BoxLayout(playersList, BoxLayout.Y_AXIS));
+    // for(String player : players) {
+    //   JLabel label = new JLabel(player);
+    //   playersList.add(label);
+    // }
+    // playlistB = playersList;
+    // JScrollPane scrollFrame = new JScrollPane(playersList);
+    // playersList.setAutoscrolls(true);
+    // frame.add(scrollFrame);
+    scrollPane.setViewportView(playersList);
+    scrollPane.setBounds(30,80,240,100);
+    frame.add(scrollPane);
+    playlistB = playersList;
+    return playersList;
+  }
+
   public void draw() {
     JFrame f = frame;  
           
     JLabel title = new JLabel("Add players");
-    title.setBounds(150, 0, 100, 50);
+    title.setBounds(150, 0, 200, 50);
     f.add(title);
+
+    addTestPlayers();
+    drawPlayerList();
+
+    JButton deletePlayer = new JButton("Delete");
+    deletePlayer.setBounds(280,90, 100, 80);
+    deletePlayer.addActionListener(new ActionListener(){  
+public void actionPerformed(ActionEvent e){  
+            // if(list1.getSelectedIndex() != -1) {                       
+            //         data = "Programming language Selected: " + list1.getSelectedValue();   
+            //         label.setText(data);  
+            //      }   
+            if(playlistB.getSelectedIndex() != -1) {
+              ((DefaultListModel) playlistB.getModel()).remove(playlistB.getSelectedIndex());
+              players.remove(playlistB.getSelectedValue());
+            } 
+        }  
+    }); 
+    f.add(deletePlayer);
 
     addBackButton();
     addHelp();
     finishDraw();
+  }
+
+  protected void addTestPlayers() {
+    players.add("svelte");
+    players.add("react");
+    players.add("vue");
+    players.add("angular");
+    players.add("angular");
+    players.add("angular");
+    players.add("angular");
+    players.add("angular");
+    players.add("angular");
+    players.add("angular");
+    players.add("angular");
+    players.add("angular");
+    players.add("angular");
+    players.add("angular");
+    players.add("angular");
   }
 }
